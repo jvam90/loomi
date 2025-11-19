eb25e730167199603eeddcf30f4f701c4b74e771
jvam90 <jvam90@gmail.com>
2025-11-19 14:45:36 -0300

    Adicionar status ao pedido

--- src/main/java/com/example/loomi/domain/Order.java
+++ src/main/java/com/example/loomi/domain/Order.java
@@ -7,11 +7,13 @@ public class Order {
     private String orderId;
     private String customerId;
     private List<Item> items;
+    private OrderStatus status;
 
-    public Order(String orderId, String customerId, List<Item> items) {
+    public Order(String orderId, String customerId, List<Item> items, OrderStatus status) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.items = items;
+        this.status = status;
     }
 
     public String getOrderId() {
@@ -38,6 +40,12 @@ public class Order {
         this.items = items;
     }
 
-    
-    
+    public OrderStatus getStatus() {
+        return status;
+    }    
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
 }
