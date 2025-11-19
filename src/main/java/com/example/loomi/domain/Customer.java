d9b7f8bf099008cd6ff0405e115a53f78e10495b
jvam90 <jvam90@gmail.com>
2025-11-19 15:50:47 -0300

    Adicionar classe customer

---
+++ src/main/java/com/example/loomi/domain/Customer.java
@@ -0,0 +1,23 @@
+package com.example.loomi.domain;
+
+public class Customer {
+    private String customerId;
+
+    public Customer() {
+    }
+
+    public Customer(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    
+    
+}
