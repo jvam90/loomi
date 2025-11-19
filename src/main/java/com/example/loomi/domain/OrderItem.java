11a46a9aa07f2944700aa4f35ef3f815c574d1cf
jvam90 <jvam90@gmail.com>
2025-11-19 16:12:30 -0300

    Adicionar classe de dominio do item do pedido (orderitem)

---
+++ src/main/java/com/example/loomi/domain/OrderItem.java
@@ -0,0 +1,63 @@
+package com.example.loomi.domain;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private Long id;
+    private Order order;
+    private Product product;
+    private Integer quantity;
+    //para snapshot de preço
+    private BigDecimal unitPrice;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Product product, Integer quantity, BigDecimal unitPrice) {
+        this.product = product;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+}
