cd5a91237520f196063ff00ebfcda6a87f72e3ce
jvam90 <jvam90@gmail.com>
2025-11-19 16:40:26 -0300

    Ajuste repositório de pedidos para buscar os pedidos pelo id do cliente

--- src/main/java/com/example/loomi/infrastructure/Repositories/OrderRepository.java
+++ src/main/java/com/example/loomi/infrastructure/Repositories/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.example.loomi.infrastructure.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.loomi.infrastructure.JPAEntities.OrderEntity;
 
+@Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, String> {
-    
+    List<OrderEntity> findByCustomerId(String customerId);
 }
