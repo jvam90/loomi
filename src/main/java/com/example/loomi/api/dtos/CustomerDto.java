6994532e3d7eefd69eee75c53bd391ad919ff204
jvam90 <jvam90@gmail.com>
2025-11-20 10:47:58 -0300

    Criação de dto de customer

---
+++ src/main/java/com/example/loomi/api/dtos/CustomerDto.java
@@ -0,0 +1,43 @@
+package com.example.loomi.api.dtos;
+
+import java.time.OffsetDateTime;
+
+public class CustomerDto {
+    private String name;
+    private String email;
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    public CustomerDto() {
+    }
+
+    public CustomerDto(String name, String email, OffsetDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
