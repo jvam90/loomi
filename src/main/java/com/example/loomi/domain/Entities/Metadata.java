c09e15d4712bd55f7f105bb2da86631ac4ab8d0f
Joao Victor Alves Martins <jvam90@gmail.com>
2025-11-21 13:19:36 -0300

    Apenas deixar valor para ReleaseDate

--- src/main/java/com/example/loomi/domain/Entities/Metadata.java
+++ src/main/java/com/example/loomi/domain/Entities/Metadata.java
@@ -1,10 +1,6 @@
 package com.example.loomi.domain.Entities;
 
 public record Metadata(
-    String releaseDate,
-    Integer preOrderSlots,
-    Integer licenses
-) {
-    
-} 
+                String releaseDate) {
 
+}
