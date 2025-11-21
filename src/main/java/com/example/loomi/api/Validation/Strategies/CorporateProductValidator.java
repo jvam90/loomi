132db6e601768c26c0c36f95c7ae7a6a466cf52d
Joao Victor Alves Martins <jvam90@gmail.com>
2025-11-21 14:54:51 -0300

    Ajuste regras de validacao de produtos corporativos

--- src/main/java/com/example/loomi/api/Validation/Strategies/CorporateProductValidator.java
+++ src/main/java/com/example/loomi/api/Validation/Strategies/CorporateProductValidator.java
@@ -23,7 +23,27 @@ public class CorporateProductValidator implements ProductValidator {
 
     @Override
     public void validate(OrderItemEntity orderItemEntity) throws ProductValidationException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+        if (!validateCustomerInformation(orderItemEntity.getOrder().getCustomerId())) {
+            throw new ProductValidationException("Invalid CNPJ and state registration for customer ID: "
+                    + orderItemEntity.getOrder().getCustomerId());
+        }
+
+        if (!validateCustomerCreditLimit(orderItemEntity.getOrder().getCustomerId())) {
+            throw new ProductValidationException("Invalid credit limit for customer ID: "
+                    + orderItemEntity.getOrder().getCustomerId());
+        }
     }
+
+    // Mock para validar CNPJ e Inscrição Estadual
+    private boolean validateCustomerInformation(String customerId) {
+        // Implementar a lógica de validação do CNPJ e Inscrição Estadual -> mock
+        return true; // Retornar true se válido, false caso contrário
+    }
+
+    // Mock para validar CNPJ e Inscrição Estadual
+    private boolean validateCustomerCreditLimit(String customerId) {
+        // Implementar a lógica de validação de limite de crédito do cliente -> mock
+        return true; // Retornar true se válido, false caso contrário
+    }
+
 }
