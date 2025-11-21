1dddf05427cc0d3cd525d857d593b46f0138980b
Joao Victor Alves Martins <jvam90@gmail.com>
2025-11-21 13:29:14 -0300

    Ajuste metodo de validacao para produtos fisicos

--- src/main/java/com/example/loomi/api/Validation/Strategies/PhysicalProductValidator.java
+++ src/main/java/com/example/loomi/api/Validation/Strategies/PhysicalProductValidator.java
@@ -6,9 +6,17 @@ import com.example.loomi.api.Validation.ProductValidationException;
 import com.example.loomi.api.Validation.ProductValidator;
 import com.example.loomi.domain.Entities.Product;
 import com.example.loomi.domain.Enums.ProductType;
+import com.example.loomi.infrastructure.JPAEntities.OrderItemEntity;
+import com.example.loomi.infrastructure.Repositories.ProductRepository;
 
 @Component
 public class PhysicalProductValidator implements ProductValidator {
+
+    private final ProductRepository productsRepository;
+
+    public PhysicalProductValidator(ProductRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
 
     @Override
     public ProductType supportedType() {
@@ -18,5 +26,19 @@ public class PhysicalProductValidator implements ProductValidator {
     @Override
     public void validate(Product product) throws ProductValidationException {
         // validações para produtos físicos
+    }
+
+    @Override
+    public void validate(OrderItemEntity orderItemEntity) throws ProductValidationException {
+        String requestedProductId = orderItemEntity.getProduct().getProductId();
+        Integer orderRequestedAmount = orderItemEntity.getQuantity();
+
+        var productEntity = productsRepository.findById(requestedProductId);
+        Integer availableStock = productEntity.get().getStockQuantity();
+
+        if (availableStock < orderRequestedAmount) {
+            throw new ProductValidationException("Insufficient stock for product ID: " + requestedProductId);
+        }
+
     }
 }
