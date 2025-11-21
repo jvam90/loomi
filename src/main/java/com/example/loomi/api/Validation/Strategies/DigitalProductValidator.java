debf967e6189785fcc5e23eb07a94eeb811ce801
Joao Victor Alves Martins <jvam90@gmail.com>
2025-11-21 16:01:18 -0300

    Adicionar validacoes de produtos digitais

--- src/main/java/com/example/loomi/api/Validation/Strategies/DigitalProductValidator.java
+++ src/main/java/com/example/loomi/api/Validation/Strategies/DigitalProductValidator.java
@@ -1,21 +1,27 @@
 package com.example.loomi.api.Validation.Strategies;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.example.loomi.api.Validation.ProductValidationException;
 import com.example.loomi.api.Validation.ProductValidator;
 import com.example.loomi.domain.Entities.Product;
 import com.example.loomi.domain.Enums.ProductType;
+import com.example.loomi.infrastructure.JPAEntities.OrderEntity;
 import com.example.loomi.infrastructure.JPAEntities.OrderItemEntity;
+import com.example.loomi.infrastructure.Repositories.OrderRepository;
 import com.example.loomi.infrastructure.Repositories.ProductRepository;
 
 @Component
 public class DigitalProductValidator implements ProductValidator {
 
     private final ProductRepository productsRepository;
+    private final OrderRepository orderRepository;
 
-    public DigitalProductValidator(ProductRepository productsRepository) {
+    public DigitalProductValidator(ProductRepository productsRepository, OrderRepository orderRepository) {
         this.productsRepository = productsRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
@@ -40,6 +46,22 @@ public class DigitalProductValidator implements ProductValidator {
             throw new ProductValidationException(
                     "Insufficient licenses in stock for product ID: " + requestedProductId);
         }
+
+        // Cliente não pode comprar o mesmo produto digital mais de uma vez
+        List<OrderEntity> customerOrders = orderRepository.findByCustomerId(
+                orderItemEntity.getOrder().getCustomerId());
+
+        if (customerOrders != null && !customerOrders.isEmpty()) {
+            for (OrderEntity order : customerOrders) {
+                for (OrderItemEntity item : order.getItems()) {
+                    if (item.getProduct().getProductId().equals(requestedProductId)) {
+                        throw new ProductValidationException(
+                                "Customer has already purchased digital product ID: " + requestedProductId);
+                    }
+                }
+            }
+        }
+
     }
 
 }
